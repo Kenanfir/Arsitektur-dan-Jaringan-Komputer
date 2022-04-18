6b2c206dfda9bfc9d11bba0ba6a4e5b328cf86f2
Kenan Firmansyah <84304315+Kenanfir@users.noreply.github.com>
2022-04-19 01:39:00 +0700

    Update PassBy.py

--- PassBy.py
+++ PassBy.py
@@ -1,17 +1,17 @@
-def PassByValue(x):
-    x = ["C"]
-    print("Isi variabel x : ", x)
-    return x
+def PassByValue(x):                 # Function dimana variabel x mengambil isi dari variabel KelasGT
+    x = ["C"]                       # x membuat variabel baru
+    print("Isi variabel x : ", x)   # Check isi variabel x
+    return x                        # Pembatas function
 
-def PassByReference(x):
-    x.append("C")
-    print("Isi variabel x : ", x)
-    return x
+def PassByReference(x):             # Function dimana variabel x mengambill isi dari variabel KelasGT dan menambahkan variabel x ke dalamnya
+    x.append("C")                   # x menggabungkan "C" kedalam KelasGT dan x itu sendiri karena x dan KelasGT merupakan variabel yang sama
+    print("Isi variabel x : ", x)   # Check isi variabel x
+    return x                        # Pembatas function
 
-KelasGT = ["A", "B"]
+KelasGT = ["A", "B"]                # Contoh variabel
 
 print("KelasGT sebelum run function : ", KelasGT)
-PassByValue(KelasGT)
+PassByValue(KelasGT)                                                   # Run function PassByValue dengan variabel KelasGT
 print("KelasGT setelah run function PassByValue : ", KelasGT)
-PassByReference(KelasGT)
+PassByReference(KelasGT)                                               # Run function PassByReference dengan variabel KelasGT
 print("KelasGT setelah run function PassByReference : ", KelasGT)
